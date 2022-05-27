66d0b6638834dca95fc4ec2ee820fda1e101d0b8
Eliúde Vemba <sweeteliude@gmail.com>
2022-05-27 23:05:30 +0100

    Initial Commit

---
+++ TwitterSwiftUIClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitterSwiftUIClone
+//
+//  Created by Eliude Vemba on 27/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterSwiftUIClone/TwitterSwiftUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUICloneApp.swift
+//  TwitterSwiftUIClone
+//
+//  Created by Eliude Vemba on 27/05/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
